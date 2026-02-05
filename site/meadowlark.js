2c8d253dbe841ae7726d524325155f17c7c380bd
Joelcio Facanha <joelciomoreira@gmail.com>
2026-02-05 12:37:54 -0300

    feat: Movendo fortune cookie para module

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const path = require("path");
+const fortune = require("./lib/fortunes");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
 
 app.engine(
   "handlebars",
@@ -24,8 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => res.render("home"));
 app.get("/about", (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render("about", { fortune: randomFortune });
+  res.render("about", { fortune: fortune.getFortune() });
 });
 
 app.use(express.static(path.join(__dirname, "public")));
